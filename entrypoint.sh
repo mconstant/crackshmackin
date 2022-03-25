c103765a4d0142982b5a0258f0f9a21202e89d70
mconstant <85532172+mconstant@users.noreply.github.com>
2022-03-24 20:51:47 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,5 @@ ruby find_satoshi.rb &
 sleep 1
 ruby get_lucky.rb &
 sleep 1
-tmux new-session -d 'tail -f /crackshmackin/data/shucks.sux'
-tmux split-window -v 'tail -f /crackshmackin/data/f.addresses'
-tmux attach
+tail -f /crackshmackin/data/shucks.sux &
+tail -f /crackshmackin/data/f.addresses &
